2b54fbf33f8a438303a268d01dfab8084378c743
mertkaynak <kaynak.mert35@gmail.com>
2023-11-07 15:15:17 +0300

    React Axios Example

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Requester from "./components/Requester";
+
+function App() {
+  return (
+    <div className="App">
+      <Requester />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Requester.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Requester = () => {
+  return <div>Requester</div>;
+};
+
+export default Requester;
